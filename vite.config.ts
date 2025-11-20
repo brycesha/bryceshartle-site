3f8759b818829201155f533df16d8461c906e047
Bryce <159097176+brycesha@users.noreply.github.com>
2025-11-20 17:22:37 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/bryceshartle-site/',   // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
